26837c26146b06ebac7c23694edc6a9a4f7569f6
hong9Lee <hongtemp9mac@gmail.com>
2022-01-26 23:21:43 +0900

    카카오 기능개발 문제 answer

--- codingTest/func_dev/Solution.java
+++ codingTest/func_dev/Solution.java
@@ -1,8 +1,7 @@
-package codingTest.func_dev;
+package func_dev;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 
 /** 카카오 level2 기능개발 문제 */
 public class Solution {
@@ -20,45 +19,60 @@ public class Solution {
         }
 
         int index = 0;
-        int count = 1;
+        int count = 0;
         int i = 1;
         int[] answer = new int[pQueue.size()];
         int queueSize = pQueue.size();
 
-        Integer p_pop = 100 - pQueue.poll();
-        int calc = p_pop / sQueue.poll();
-        while(i < queueSize) {
-            int p = pQueue.poll();
-            int s = sQueue.poll();
+
+        int flag = 0;
+        // 95, 90, 99, 99, 80, 99           1,1,1,1,1
+
+        int p = pQueue.peek(); // 95
+        int s = sQueue.peek(); // 1
+
+        Integer p_pop = 100 - p; // 5
+        int calc = p_pop / s; //   5/1 => 5
+        while(true) {
+
+            if(i == queueSize){
+                answer[index] = count;
+                answer[index+1] = 1;
+                break;
+            }
 
             if( (calc * s + p) >= 100) {
                 count++;
+
+
+
+                pQueue.poll();
+                sQueue.poll();
+
+
+
+                p = pQueue.peek();
+                s = sQueue.peek();
+
             }
             else {
 
                 answer[index] = count;
+
                 p_pop = 100 - p;
                 calc = p_pop / s;
+
+
+
+                pQueue.poll();
+                sQueue.poll();
+
                 count = 1;
                 index++;
-                if(i + 1 == queueSize) answer[index] = count;
             }
             i++;
 
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
         return answer;
@@ -78,7 +92,7 @@ public class Solution {
 //        if(pQueue.poll() == null) System.out.println("#");
 //        if(pQueue.poll() == null) System.out.println("%%");
 
-
+        System.out.println("@@");
 //        solution.solution(new int[]{93, 30, 55}, new int[]{1, 30, 5});
         solution.solution(new int[]{95, 90, 99, 99, 80, 99}, new int[]{1, 1, 1, 1, 1, 1});
     }
